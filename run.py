6429653a48fc90a871e4b48eaadf3d17f63993e8
Armando Urquiola Cabrera <patchamama.go@gmail.com>
2023-04-19 16:13:09 +0000

    Add menu and actions to 'Reset Conditions', 'Exceute Query' and some corrections with query

--- run.py
+++ run.py
@@ -120,37 +120,45 @@ def clear_terminal():
 
 def show_menu(opt):
     global catalog_data
+    if (len(catalog_data) == 0):
+        catalog_data = get_all_records(catalog)
     cond_total = []
+    and_operator = True
     while True:
         if opt=="1": #Search a book > filters
             clear_terminal()
-            print('|====== SEARCH A BOOK (FILTER) =======================')
-            print('|----- One condition (simple) ------------------------')
-            print('|      1. Search in any field (author or title)')
-            print('|      2. Search a book for ID-Number')
-            print('|----- Multiple conditions (<AND> operator) -----------')
-            print('|      3. Add a author condition')
-            print('|      4. Add a title condition')
-            print('|      5. Add a language condition')
-            print('|      6. Change operator to <OR>')
-            print('|------------------------------------------------------')
-            print('|      7. Return to the main menu')
-            print('|------------------------------------------------------')
-            print(cond_total)
+            print('|====== MAIN > SEARCH A BOOK (FILTER) ================')
+            if len(cond_total) > 0:
+                print(f"| Conditions: {cond_total}")
+            else:
+                print("|      No conditions")
+            print( '|----- One condition (simple + Conditions reset) ------')
+            print( '|      1. Search in any field (author or title)')
+            print( '|      2. Search a book for ID-Number')
+            print( '|----- Multiple conditions (<AND> operator) -----------')
+            print( '|      3. Add a author condition')
+            print( '|      4. Add a title condition')
+            print( '|      5. Add a language condition')
+            print( '|------------------------------------------------------')
+            print(f'|      6. Change operator to <{"OR" if (and_operator) else "AND"}>')
+            print( '|      7. Reset conditions')
+            print( '|      8. Show results')
+            print( '|------------------------------------------------------')
+            print( '|      9. Return to the main menu')
+            print( '|------------------------------------------------------')
             opt_menu = input('| Select a option?\n')
             if opt_menu == "1": #any field
-                search_cond = input("Enter the author or field to search?\n")
+                search_cond = input("Enter the author or title to search?\n")
                 if len(search_cond) > 0:
-                    if (len(catalog_data)==0):
-                        catalog_data = get_all_records(catalog)
-                    cond_total.append([{"Authors": search_cond}, {"Title": search_cond}, {"<operator>": "or"}])
-                    filtered_data = get_filter_data(catalog_data, [{"Authors": search_cond}, {"Title": search_cond}], False)
-                    if (len(filtered_data)==0):
-                        pause("No data found with ID={Id}")
+                    filtered_data = catalog_data #Reset all the conditions and use as input all the data of the catalog
+                    cond_total = [{"Authors": search_cond}, {"Title": search_cond}, {"<operator>": "or"}]
+                    filtered_data = get_filter_data(filtered_data, [{"Authors": search_cond}, {"Title": search_cond}], False)
+                    if (len(filtered_data) == 0):
+                        pause(f"No data found with title or author={search_cond}")
                     else:
                         print_data(filtered_data)
                         pause()
-            if opt_menu == "2": #ID
+            elif opt_menu == "2": #Search a ID
                 search_cond = input("Enter the ID to search?\n") 
                 try:
                     Id = int(search_cond)
@@ -158,22 +166,43 @@ def show_menu(opt):
                     pause("Error: the ID is not a number, please enter a number integer to search...")
                     continue
                 if len(search_cond) > 0:
-                    cond_total.append([{"Authors": search_cond}, {"Title": search_cond}, {"<operator>": "and"}])
-                    if (len(catalog_data)==0):
-                        catalog_data = get_all_records(catalog)
-                    filtered_data = get_filter_data(catalog_data, [{"Text#": Id}])
+                    filtered_data = catalog_data #Reset all the conditions and use as input all the data of the catalog
+                    cond_total = [{"Text#": Id}, {"<operator>": 'and' if (and_operator) else 'or'}]
+                    filtered_data = get_filter_data(filtered_data, [{"Text#": Id}])
                     if (len(filtered_data)==0):
                         pause(f"No data found with ID={Id}")
                     else:
                         print_data(filtered_data)
                         pause()
-            elif opt_menu =="7": #Return to the main menu
+            elif opt_menu =="3": #Add a author condition
+                pass
+            elif opt_menu =="4": #Add a title condition
+                pass
+            elif opt_menu =="5": #Add a language condition
+                pass
+            elif opt_menu =="6": #Switch operator <or> / <and> 
+                and_operator = not and_operator
+                pause(f"Operator changed to <{'and' if (and_operator) else 'or'}>...")           
+            elif opt_menu =="7": #Reset all the conditions
+                filtered_data = catalog_data #Reset all the conditions and use as input all the data of the catalog
+                cond_total = []
+                pause("All conditions reseted...")
+            elif opt_menu =="8": #Execute query with conditions
+                if (len(cond_total)>0):
+                    filtered_data = get_filter_data(filtered_data, cond_total)
+                    if (len(filtered_data)==0):
+                        pause(f"No data found with with conditions: {cond_total}")
+                    else:
+                        print_data(filtered_data)
+                        pause()
+                    pass
+                else:
+                    pause("First select some conditions to show some result")
+            elif opt_menu =="9": #Return to the main menu
                 break
-        elif opt=="2": #Search and send ebook
-            break
+        if opt=="2": #Send ebook to mail
             pass
-        elif opt=="3": #Show statistics of request
-            break
+        if opt=="3": #See Statistics of request
             pass
         else:
             pause(f'Error: Unknown option selected "{opt}"')
